8e807835e9d5469a1d5a7e9f1759e0476e23b30f
Raghu K <raghuk.vit@gmail.com>
2022-08-04 07:15:13 +0530

    Update terraform/tf-module-rds/

---
+++ schema.tf
@@ -0,0 +1,10 @@
+resource "null_resource" "schema" {
+  provisioner "local-exec" {
+    command = <<EOF
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql  -h ${aws_db_instance.main.address} -u${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_USER"]} -p${jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["RDS_MYSQL_PASS"]}
+EOF
+  }
+}
